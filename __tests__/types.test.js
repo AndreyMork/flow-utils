7fe16ab182af126cf4f5dc64aa5f1232aec49dc4
Andrey <magot.a.d@gmail.com>
2019-02-01 09:14:46 +0300

    disable test

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -6,7 +6,7 @@ const app = require('../src').default;
 
 const pathToFixtures = path.join(__dirname, '../__fixtures__/');
 
-test('test', () => {
+test.skip('test', () => {
   const sourceFileContent = fs.readFileSync(path.join(pathToFixtures, 'source.js'), 'utf-8');
   const distFileContent = fs.readFileSync(path.join(pathToFixtures, 'dist.js'), 'utf-8');
 
