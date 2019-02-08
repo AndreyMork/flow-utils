274aec85cc481ef50aea25c3209f944840855645
Andrey <magot.a.d@gmail.com>
2019-02-09 01:48:06 +0300

    chore

--- src/Type/types.flow.js
+++ src/Type/types.flow.js
@@ -2,23 +2,8 @@
 
 export type BabelTypesTypeAnnotationType = {};
 
-// export type TypesType =
-//   // SimpleType
-//   | {| type: string |}
-//   // ValueType
-//   | {|
-//       type: string,
-//       value: string | number | boolean,
-//     |}
-//   // TupleType
-//   | {| type: string, elements: $ReadOnlyArray<TypesType> |}
-//   // ObjectType
-//   | {|
-//       type: string,
-//       properties: $ReadOnlyArray<{| key: string, value: TypesType |}>,
-//     |};
-
 export type SimpleTypeType = {| type: string |};
+
 export type ValueTypeType = {|
   type: string,
   value: string | number | boolean,
